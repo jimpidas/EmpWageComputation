a3d672dd7705ff77ebb1f5f3f422239775e14ec4
unknown <jimpidas>
2021-04-06 11:55:12 +0530

    Added UC4

---
+++ UsingSwitchCase.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+empWagePerHr=20
+isFulltime=1
+isParttime=2
+empWorkingHrs=0
+randomCheck=$((RANDOM%3))
+
+case $randomCheck in
+        $isFulltime)
+               empWorkingHrs=8
+                echo "Employee is Fulltime present"
+                ;;
+
+        $isParttime)
+                empWorkingHrs=4
+                echo "Employee is parttime present"
+                ;;
+        *)
+                echo "Employee is absent"
+                ;;
+esac
+
+DailyWage=$(($empWagePerHr*$empWorkingHrs))
+
+
