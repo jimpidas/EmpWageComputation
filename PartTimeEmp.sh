e6731d676a982f2430b0464ee77e5b56905c7d99
unknown <jimpidas>
2021-04-06 11:53:49 +0530

    added UC3

---
+++ PartTimeEmp.sh
@@ -0,0 +1,23 @@
+empWagePerHr=20
+isFulltime=1
+isParttime=2
+empWorkingHrs=0
+randomCheck=$((RANDOM%3))
+
+if [ $isFulltime -eq $randomCheck ]
+then
+        empWorkingHrs=8
+        echo "Employee is Fulltime present"
+
+elif [ $isParttime -eq $randomCheck ]
+then
+        empWorkingHrs=4
+        echo "Employee is parttime present"
+
+else
+        echo "Employee is absent"
+
+fi
+
+DailyWage=$(($empWagePerHr*$empWorkingHrs))
+echo $DailyWage
